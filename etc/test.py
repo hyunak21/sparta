9fc936ace68d7f5ec88981f834a9918ac2720285
hyunak <hyunak21@hotmail.com>
2020-05-23 16:50:17 +0900

    a

---
+++ etc/test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*- 
+
+import requests 
+
+r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+rjson = r.json()
+print(rjson['RealtimeCityAir']['row'][0]['NO2'])
